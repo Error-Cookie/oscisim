0aca8d911a6533af4cd638de3deae516436d56ac
Error-Cookie <cookiegamer1780@gmail.com>
2024-01-30 18:06:13 +0100

    GH Link component

---
+++ src/components/Info.tsx
@@ -0,0 +1,9 @@
+export default function Info() {
+	return (
+		<div className="ml-3">
+			<a href="https://github.com/Error-Cookie/oscisim" target="_blank" className="flex-none">
+				<img src="/github.svg" className="opacity-35" width={50} height={50} />
+			</a>
+		</div>
+	);
+}
