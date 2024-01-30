76fbd9f8af07956cad6eac7356e7aa306cbe1219
Error-Cookie <cookiegamer1780@gmail.com>
2024-01-30 18:04:47 +0100

    Add remaining oscillators

--- src/components/OscillatorAnimation.tsx
+++ src/components/OscillatorAnimation.tsx
@@ -3,6 +3,9 @@ import Box from "./Box";
 import Pendulum from "./oscillators/Pendulum";
 import { OsciCtx } from "../App";
 import SpringPendulum1 from "./oscillators/SpringPendulum1";
+import SpringPendulum2 from "./oscillators/SpringPendulum2";
+import BalanceWheel from "./oscillators/BalanceWheel";
+import Chord from "./oscillators/Chord";
 
 export default function OscillatorAnimation() {
 	const osciCtx = useContext(OsciCtx);
@@ -16,11 +19,11 @@ export default function OscillatorAnimation() {
 				) : oscillator == "Federpendel 1" ? (
 					<SpringPendulum1 />
 				) : oscillator == "Federpendel 2" ? (
-					<></>
+					<SpringPendulum2 />
 				) : oscillator == "Unruh" ? (
-					<></>
+					<BalanceWheel />
 				) : oscillator == "Saite" ? (
-					<></>
+					<Chord />
 				) : (
 					<></>
 				)}
